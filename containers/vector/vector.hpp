c37ba22d3c37a79d0af4276035a64420c5cb3ca0
ADEK iMac Pro 1 <adek@ADEKs-iMac-Pro.local>
2023-05-09 13:58:31 +0400

    Changes: 	- added default vector constructor 	- added fill vector constructor

--- containers/vector/vector.hpp
+++ containers/vector/vector.hpp
@@ -6,20 +6,21 @@
 /*   By: kamin <kamin@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 16:33:29 by kamin             #+#    #+#             */
-/*   Updated: 2023/04/05 17:48:17 by kamin            ###   ########.fr       */
+/*   Updated: 2023/05/09 13:57:39 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
+#include <memory>
 namespace ft
 {
     /**
      * @brief This is a replica of the vector container in STL c++98
-     *
+     * 
      * usage: ft::vector
-     *
+     *  
      * (constructor):        Construct list
      *  (1) empty container constructor (default constructor)
      *      Constructs an empty container, with no elements.
@@ -31,6 +32,7 @@ namespace ft
      *      Constructs a container with a copy of each of the elements in x, in the same order.
      * (destructor):         Destruct list
      * operator=:            Assign list
+     * 
      * - Iterators:
      * begin:                Return iterator to beginning (public member function)
      * end:                  Return iterator to end (public member function)
@@ -68,10 +70,89 @@ namespace ft
      * swap:                 Exchanges contents of two lists (function template)
      *
      */
-    template <typename T> class vector
+    template < class T, class Alloc = std::allocator<T> > class vector
     {
+        public:
+            /**
+			 * The first template parameter (T)
+			*/
+			typedef T                                           value_type;
+
+			/*
+			** The second template parameter (Alloc)
+			*/
+			typedef Alloc                                       allocator_type;
+            /*
+			** allocator_type::reference
+			** A type provides a reference to an element stored in
+			** a vector.
+			** For the default allocator is a reference to value_type
+			** (value_type&)
+			*/
+			typedef typename allocator_type::reference          reference;
+
+			/*
+			** allocator_type::const_reference
+			** Type that give a reference to a const element stored.
+			** Usefull to read and perform const operator.
+			** A type const_reference can't be used to modify the value
+			** of an element.
+			** For the default allocator is a const reference to value_type
+			** (const value_type&)
+			*/
+			typedef typename allocator_type::const_reference    const_reference;
+			
+			/*
+			** allocator_type::pointer
+			** Type that give a pointer to an element stored.
+			** A type pointer can be used to modify the value of
+			** an element.
+			** For the default allocator is a pointer to value_type
+			** (value_type*)
+			*/
+			typedef typename allocator_type::pointer            pointer;
+
+			/*
+			** allocator_type::const_pointer
+			** Type that give a const pointer to an element stored.
+			** Can't be used to modify the value of an element.
+			** An iterator is prefered to access to an element.
+			** For the default allocator is a const pointer to value_type
+			** (const value_type*)
+			*/
+			typedef typename allocator_type::const_pointer      const_pointer;
+            /*
+			** An unsigned integral type that can represent any
+			** non-negative value of difference_type
+			** Usually the same as size_t.
+			*/
+			typedef typename allocator_type::size_type          size_type;
+
+            
+            explicit vector (const allocator_type& alloc = allocator_type()): _allocator(alloc), _start(nullptr), _end(nullptr), _end_capacity(nullptr)
+            { }
+            explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): _allocator(alloc), _start(nullptr), _end(nullptr), _end_capacity(nullptr)
+            {
+                _start = _allocator.allocate(n);
+                _end = _start;
+                _end_capacity = _start + n;
+                
+                while (n--)
+                {
+                    _allocator.construct(_end, val);
+                    _end++;
+                }
+            }
+            ~vector()
+            {
+
+            }
         private:
-            sizt_t  capacity; //grows exponentially where it doubles everytime at least for clang
+            allocator_type  _allocator;
+			pointer         _start;
+			pointer         _end;
+			pointer         _end_capacity;
+            size_t          _capacity; //grows exponentially where it doubles everytime at least for clang
     };
 }
 #endif
